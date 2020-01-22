cadce5172fca8a3e907941dc0ba446e16849c6f0
Brian Hague <alphaseinor@gmail.com>
2020-01-21 20:48:22 -0600

    all get endpoints implemented

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -8,8 +8,8 @@ const router = express.Router()
 router.get('/', (req,res) => {
   console.log(req.query)
   db.find(req.query)
-  .then(post => {
-    res.status(200).json(post)
+  .then(posts => {
+    res.status(200).json(posts)
   })
   .catch(error => {
     res.status(500).json({
@@ -18,5 +18,26 @@ router.get('/', (req,res) => {
   })
 })
 
+router.get('/:id', (req,res) => {
+  db.findById(req.params.id)
+  .then(post => {
+    post ? res.status(200).json(post) : res.status(404).json({
+      message: "The post with the specified ID does not exist."
+    })
+  })
+  .catch(error => {
+    res.status(500).json({error: "The post information could not be retrieved."})
+  })
+})
+
+router.get('/:id/comments', (req,res) => {
+  db.findPostComments(req.params.id)
+  .then(comments => {
+    comments ? res.status(200).json(comments) : res.status(404).json({message: "The post with the specified ID does not exist."})
+  })
+  .catch(error => {
+    res.status(500).json({error: "The comments information could not be retrieved."})
+  })
+})
 
 module.exports = router
