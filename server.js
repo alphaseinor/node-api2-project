f06a9c71cdf313bf67d1b7027a786e68f19cea1b
Brian Hague <alphaseinor@gmail.com>
2020-01-23 07:23:48 -0600

    did cors as stretch goal

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const postsRouter = require('./posts/posts-router.js')
+const cors = require('cors')
 
 const server = express()
 
 server.use(express.json())
+server.use(cors())
 
 server.use('/api/posts', postsRouter)
 
